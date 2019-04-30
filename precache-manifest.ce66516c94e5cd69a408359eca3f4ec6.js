b354af585ba8d40a0a19b938812b70c859541b6a
unres <timothy.bell@ttu.edu>
2019-04-30 15:57:36 -0500

    Updates

---
+++ precache-manifest.ce66516c94e5cd69a408359eca3f4ec6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "09f51a2f85c9fc65c59c",
+    "url": "/static/js/main.c282f97b.chunk.js"
+  },
+  {
+    "revision": "338e7f9a4bfb52227a97",
+    "url": "/static/js/2.a872fea1.chunk.js"
+  },
+  {
+    "revision": "09f51a2f85c9fc65c59c",
+    "url": "/static/css/main.ce0ce5a3.chunk.css"
+  },
+  {
+    "revision": "cadf92fbbda262d5226b7980704f710d",
+    "url": "/index.html"
+  }
+];
