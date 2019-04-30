c8e484a8ab91a8de5cda555fe8b4944eeb1fca81
unres <timothy.bell@ttu.edu>
2019-04-30 16:00:15 -0500

    Deploying V.1

---
+++ build/precache-manifest.ba1e54e8d0f15c114b5f084b4ab5746a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5a5e4e949ee2e626d127",
+    "url": "/static/js/main.38561a09.chunk.js"
+  },
+  {
+    "revision": "338e7f9a4bfb52227a97",
+    "url": "/static/js/2.a872fea1.chunk.js"
+  },
+  {
+    "revision": "5a5e4e949ee2e626d127",
+    "url": "/static/css/main.ce0ce5a3.chunk.css"
+  },
+  {
+    "revision": "65e014439133389905c641cdac9a2ad6",
+    "url": "/index.html"
+  }
+];
--- src/components/final.js
+++ src/components/final.js
@@ -1,27 +1,70 @@
 import React from "react";
 import axios from "axios";
-import { Dropdown, Form, Button, Header, Card, Grid } from "semantic-ui-react";
+import { Button, Header, Card, Grid } from "semantic-ui-react";
 
 class Final extends React.Component {
-  callAPI() {
+  state = { name: null, address: null };
+  //calls the backend on the cloud to obtain plae info
+  callAPI = props => {
     axios
-      .post("localhost:8000", { data: this.state })
+      .post(process.env.REACT_APP_API_ENDPOINT, {
+        data: this.props.values
+      })
       .then(data => {
+        this.setState({ name: this.data.name, address: this.data.address });
         console.log(data);
         console.log("Success!");
       })
       .catch(err => {
         console.error(err);
       });
-  }
-  render() {
-    const { values } = this.props;
-    console.log(this.props);
+  };
+
+  //renders the button or the address/name
+  renderContent() {
+    if (!this.state.name) {
+      return (
+        <Grid
+          verticalAlign="middle"
+          centered
+          style={{ height: "25vh", marginTop: "25vh" }}
+        >
+          <Card>
+            <Card.Header as="h1" textAlign="center">
+              Find Food
+            </Card.Header>
+            <Card.Content>
+              <Button onClick={this.callAPI()} style={{ marginTop: "10px" }}>
+                Find Food!
+              </Button>
+            </Card.Content>
+          </Card>
+        </Grid>
+      );
+    }
     return (
-      <Button onClick={this.callAPI} style={{ marginTop: "10px" }}>
-        Save And Continue{" "}
-      </Button>
+      <Grid
+        verticalAlign="middle"
+        centered
+        style={{ height: "25vh", marginTop: "25vh" }}
+      >
+        <Card>
+          <Card.Header as="h1" textAlign="center">
+            Find Food
+          </Card.Header>
+          <Card.Content>
+            <Header as="h2" style={{ marginTop: "15%" }}>
+              {this.state.name}
+            </Header>
+            <Card.Description>{this.state.address}</Card.Description>
+          </Card.Content>
+        </Card>
+      </Grid>
     );
+  }
+
+  render() {
+    return <div>{this.renderContent()}</div>;
   }
 }
 
