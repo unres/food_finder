d5f9b15fa046f19550e32efe929dcd9330bbd625
unres <timothy.bell@ttu.edu>
2019-04-30 16:06:19 -0500

    Deploying V.1.2

---
+++ build/precache-manifest.d5671afa8ab9190fa692993cc614b5f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f4d6332c867a7a4316c",
+    "url": "/food_finder/static/js/runtime~main.af2a95db.js"
+  },
+  {
+    "revision": "5a5e4e949ee2e626d127",
+    "url": "/food_finder/static/js/main.38561a09.chunk.js"
+  },
+  {
+    "revision": "338e7f9a4bfb52227a97",
+    "url": "/food_finder/static/js/2.a872fea1.chunk.js"
+  },
+  {
+    "revision": "5a5e4e949ee2e626d127",
+    "url": "/food_finder/static/css/main.ce0ce5a3.chunk.css"
+  },
+  {
+    "revision": "6b92047f3f65bc17ace5c51f75d846f8",
+    "url": "/food_finder/index.html"
+  }
+];
